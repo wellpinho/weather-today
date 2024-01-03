68c3242216d8b4578df1ed15e6ac9030d941f4bc
gustavo <gustavorodri.dev@gmail.com>
2024-01-02 21:22:34 -0300

    direcionamento da pagina contact

---
+++ src/pages/contact.tsx
@@ -0,0 +1,12 @@
+
+const contact = () => {
+    return (
+        <div>
+            <h1>
+                Page de contact em construçao
+            </h1>
+        </div>
+    )
+}
+
+export default contact
